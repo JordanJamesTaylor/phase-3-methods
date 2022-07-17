bede5614ae550941ebdd3ce7e780cb6b4c04086d
jordan <jordjamestaylor@gmail.com>
2022-07-17 16:56:40 -0400

    commit tree ac9af2fb578fc301cb8c0f2c0241ef68a7936fd5

--- methods.rb
+++ methods.rb
@@ -1,1 +1,38 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+greet_with_default
+greet_with_default("Sunny")
+
+def add(num1, num2)
+    puts num1 + num2
+    num1 + num2
+end
+
+add(1, 2)
+
+def halve(num1, num2 = 2)
+    if num1.is_a? String
+        return nil
+    end
+
+    return num1 / num2
+end
+
+puts halve(10)
+
+halve(10)
+
